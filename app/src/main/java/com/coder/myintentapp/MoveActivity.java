3b472beb7d8bb6e9bae804ab9d65c6ddfff8e52d
nurimammasri <nurimammasri.01@gmail.com>
2020-04-26 15:52:27 +0800

    Intent

---
+++ app/src/main/java/com/coder/myintentapp/MoveActivity.java
@@ -0,0 +1,15 @@
+package com.coder.myintentapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dicoding.picodiploma.myintentapp.R;
+
+public class MoveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move);
+    }
+}
